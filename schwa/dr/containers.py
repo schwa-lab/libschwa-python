d6573fc4c393c23513bb843d5caaf6f613744564
Joel Nothman <joel.nothman@gmail.com>
2012-12-05 13:43:10 +1100

    Return the corresponding slice from StoreList.create_n

--- schwa/dr/containers.py
+++ schwa/dr/containers.py
@@ -17,11 +17,14 @@ class StoreList(list):
     del self[:]
 
   def create(self, **kwargs):
+    """Instantiate, append and return an object of this store's klass"""
     obj = self._klass(**kwargs)
     self.append(obj)
     return obj
 
   def create_n(self, n, **kwargs):
+    """Instantiate and append n objects of this store's klass, returning the corresponding slice"""
     for i in xrange(n):
       obj = self._klass(**kwargs)
       self.append(obj)
+    return slice(len(self) - n, len(self))
