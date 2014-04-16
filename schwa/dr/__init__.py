3243b3d9caf076aace2ac53c3f77baa9485a3ca4
Tim Dawborn <tim.dawborn@gmail.com>
2014-04-17 09:23:01 +1000

    Added missing WriterException import in dr/__init__.py

--- schwa/dr/__init__.py
+++ schwa/dr/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import absolute_import, print_function, unicode_literals
 from .containers import StoreList
 from .decoration import Decorator, decorator, method_requires_decoration, requires_decoration
-from .exceptions import DependencyException, ReaderException
+from .exceptions import DependencyException, ReaderException, WriterException
 from .fields_core import Field, Pointer, Pointers, SelfPointer, SelfPointers, Slice, Store
 from .fields_extra import DateTime, Text
 from .meta import Ann, Doc, make_ann
@@ -13,4 +13,4 @@ from .writer import Writer
 from . import decorators
 
 
-__all__ = ['StoreList', 'Decorator', 'decorator', 'decorators', 'requires_decoration', 'method_requires_decoration', 'DependencyException', 'ReaderException', 'Field', 'Pointer', 'Pointers', 'SelfPointer', 'SelfPointers', 'Slice', 'Store', 'DateTime', 'Text', 'Ann', 'Doc', 'make_ann', 'Token', 'Reader', 'Writer']
+__all__ = ['StoreList', 'Decorator', 'decorator', 'decorators', 'requires_decoration', 'method_requires_decoration', 'DependencyException', 'ReaderException', 'Field', 'Pointer', 'Pointers', 'SelfPointer', 'SelfPointers', 'Slice', 'Store', 'DateTime', 'Text', 'Ann', 'Doc', 'make_ann', 'Token', 'Reader', 'Writer', 'WriterException']
