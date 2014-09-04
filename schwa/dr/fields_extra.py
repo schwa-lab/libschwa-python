f661bf7d1d29fc3255269b5599589d30864c186a
Tim Dawborn <tim.dawborn@gmail.com>
2014-09-04 11:13:33 +1000

    Deprecated dr.Text.

--- schwa/dr/fields_extra.py
+++ schwa/dr/fields_extra.py
@@ -1,6 +1,7 @@
 # vim: set et nosi ai ts=2 sts=2 sw=2:
 # coding: utf-8
 from __future__ import absolute_import, print_function, unicode_literals
+import warnings
 
 import dateutil.parser
 import six
@@ -31,6 +32,7 @@ class Text(Field):
   __slots__ = ('encoding', )
 
   def __init__(self, encoding='utf-8', store_empty=False, **kwargs):
+    warnings.warn('`dr.Text` is deprecated as of version 0.3.3. Using `dr.Field` with unicode and byte types now works properly.', DeprecationWarning)
     super(Text, self).__init__(**kwargs)
     self.encoding = encoding
     if not store_empty:
